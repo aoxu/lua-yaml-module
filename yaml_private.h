de60991545e4a2a57ce8a97770d5a926febe1516
XA <xatest@gmail.com>
2014-02-11 20:56:21 +0800

    fixed compilation error on ubuntu 12.04 LTS x86_64

--- yaml_private.h
+++ yaml_private.h
@@ -3,7 +3,12 @@
 #include <config.h>
 #endif
 
-#include <yaml.h>
+#define YAML_VERSION_MAJOR 0
+#define YAML_VERSION_MINOR 1
+#define YAML_VERSION_PATCH 5
+#define YAML_VERSION_STRING "0.1.5"
+
+#include "yaml.h"
 
 #include <assert.h>
 #include <limits.h>
